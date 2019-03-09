219db77813143423807b5919cd3fc0990488f19b
王梓祺 <ziqiwang@wirelessprv-10-194-97-145.near.illinois.edu>
2019-03-09 12:57:49 -0600

    Create ViewTeamsActivity

---
+++ BallTracker/app/src/main/java/com/shan/howard/balltracker/ViewTeamsActivity.java
@@ -0,0 +1,14 @@
+package com.shan.howard.balltracker;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewTeamsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_teams);
+    }
+}
