dc9a506b2d5759d2f01929cf811bbd0285b2376e
Steven Sung <sysung98@gmail.com>
2019-03-18 14:48:38 -0500

    Adding Review Game

--- BallTracker/app/src/main/java/com/shan/howard/balltracker/ReviewGameActivity.java
+++ BallTracker/app/src/main/java/com/shan/howard/balltracker/ReviewGameActivity.java
@@ -1,20 +1,32 @@
 package com.shan.howard.balltracker;
 
 import android.app.Activity;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.shan.howard.balltracker.datamodels.Game;
+import com.shan.howard.balltracker.datamodels.Team;
+import com.shan.howard.balltracker.viewmodels.GameViewModel;
+import com.shan.howard.balltracker.viewmodels.TeamViewModel;
+
+import java.util.List;
+
 import static android.content.ContentValues.TAG;
 
-public class ReviewGameActivity extends Activity implements View.OnClickListener{
+public class ReviewGameActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button mainMenu_btn;
     EditText search_et;
     ListView teams_lv;
+    GameViewModel mGameViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,15 @@ public class ReviewGameActivity extends Activity implements View.OnClickListener
         mainMenu_btn.setOnClickListener(this);
         search_et = findViewById(R.id.searchET);
         teams_lv = findViewById(R.id.view_teams_teams_lv);
+
+        mGameViewModel = ViewModelProviders.of(this).get(GameViewModel.class);
+        mGameViewModel.getAllGames().observe(this, new Observer<List<Game>>() {
+            @Override
+            public void onChanged(@Nullable final List<Game> aGames) {
+                Log.d(TAG, "New games size: " + aGames.size());
+
+            }
+        });
     }
 
     @Override
